d71e3da30279e8b011fd830afea48057380038ba
dnisaev <dnisaev@yandex.ru>
2024-05-31 17:34:59 +0300

    update folders for chat-back

---
+++ chat/src/App.tsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import './App.css'
+
+export function App() {
+  const [messages, setMessages] = useState([
+    {message: "hello!", id: "1", user: {id: "1", name: "dnisaev"}},
+    {message: "everybody!", id: "2", user: {id: "1", name: "dnisaev"}},
+    {message: "hi!", id: "3", user: {id: "2", name: "comrus"}},
+  ])
+
+  return (
+      <>
+        <div>
+          {
+            messages.map((m, index) => (
+                <div key={index}>
+                  <b>{m.user.name}:</b> {m.message}
+                  <hr/>
+                </div>
+            ))
+          }
+        </div>
+        <textarea style={{display: "block"}}>
+
+        </textarea>
+        <button>Отправить</button>
+      </>
+
+  )
+}
