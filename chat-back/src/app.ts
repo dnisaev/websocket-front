575c2dfd32576025a0146e5f39649a1f08e9b243
dnisaev <dnisaev@yandex.ru>
2024-06-03 13:46:39 +0300

    update app.ts

---
+++ chat-back/src/app.ts
@@ -0,0 +1,20 @@
+import express from 'express';
+import {createServer} from 'node:http';
+import {join} from 'node:path';
+import {Server} from 'socket.io';
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+    res.sendFile(join(__dirname, '../../chat/public/index.html'));
+});
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+});
+
+server.listen(3009, () => {
+    console.log('server running at http://localhost:3009');
+});
