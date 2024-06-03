d893c83aeaa6167153a982a3810fec6b3e1c18ee
dnisaev <dnisaev@yandex.ru>
2024-06-03 13:04:09 +0300

    remove unnecessary files

--- chat/src/App.tsx
+++ chat/src/App.tsx
@@ -3,32 +3,27 @@ import './App.css'
 
 export function App() {
   const [messages, setMessages] = useState([
-    {message: "hello!", id: "1", user: {id: "1", name: "dnisaev"}},
-    {message: "everybody!", id: "2", user: {id: "1", name: "dnisaev"}},
-    {message: "hi!", id: "3", user: {id: "2", name: "comrus"}},
+    { message: 'hello!', id: '1', user: { id: '1', name: 'dnisaev' } },
+    { message: 'everybody!', id: '2', user: { id: '1', name: 'dnisaev' } },
+    { message: 'hi!', id: '3', user: { id: '2', name: 'comrus' } },
   ])
 
   function handleClick() {
-    setMessages([...messages, {message: "ok!", id: "3", user: {id: "2", name: "comrus"}}])
+    setMessages([...messages, { message: 'ok!', id: '3', user: { id: '2', name: 'comrus' } }])
   }
 
   return (
-      <>
-        <div>
-          {
-            messages.map((m, index) => (
-                <div key={index}>
-                  <b>{m.user.name}:</b> {m.message}
-                  <hr/>
-                </div>
-            ))
-          }
-        </div>
-        <textarea style={{display: "block"}} >
-
-        </textarea>
-        <button onClick={handleClick}>Отправить</button>
-      </>
-
+    <>
+      <div>
+        {messages.map((m, index) => (
+          <div key={index}>
+            <b>{m.user.name}:</b> {m.message}
+            <hr />
+          </div>
+        ))}
+      </div>
+      <textarea style={{ display: 'block' }}></textarea>
+      <button onClick={handleClick}>Отправить</button>
+    </>
   )
 }
