a3a7dd983487abd49d5026b4eb7adac98cf5e7d8
dnisaev <dnisaev@yandex.ru>
2024-06-04 14:02:29 +0300

    update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,28 @@
+import { useState } from 'react'
+
 export function App() {
-  return <div>Hello</div>
+  const [messages, setMessages] = useState([
+    { id: '1', message: 'hello!', user: { id: '1', name: 'dnisaev' } },
+    { id: '2', message: 'everybody!', user: { id: '1', name: 'dnisaev' } },
+    { id: '3', message: 'hi!', user: { id: '2', name: 'comrus' } },
+  ])
+
+  function handleClick() {
+    setMessages([...messages, { id: '3', message: 'ok!', user: { id: '2', name: 'comrus' } }])
+  }
+
+  return (
+    <>
+      <div>
+        {messages.map((m, index) => (
+          <div key={index}>
+            <b>{m.user.name}:</b> {m.message}
+            <hr />
+          </div>
+        ))}
+      </div>
+      <textarea style={{ display: 'block' }}></textarea>
+      <button onClick={handleClick}>Отправить</button>
+    </>
+  )
 }
